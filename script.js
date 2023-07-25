f26f9b564fe25e0b2dd09396dcd7baf578d69780
KBiz248 <129792898+KBiz248@users.noreply.github.com>
2023-07-25 17:42:29 -0400

    attach functions to number buttons

---
+++ script.js
@@ -0,0 +1,60 @@
+let total = 0;
+
+function one(){
+    total = total*10+1;
+    console.log(total);
+}
+
+function two(){
+    total=total*10+2;
+    console.log(total);
+}
+
+function three(){
+    total=total*10+3;
+    console.log(total);
+}
+
+function four(){
+    total=total*10+4;
+    console.log(total);
+}
+
+function five(){
+    total=total*10+5;
+    console.log(total);
+}
+
+function six(){
+    total=total*10+6;
+    console.log(total);
+}
+
+function seven(){
+    total=total*10+7;
+    console.log(total);
+}
+
+function eight(){
+    total=total*10+8;
+    console.log(total);
+}
+
+function nine(){
+    total=total*10+9;
+    console.log(total);
+}
+
+function zero(){
+    total=total*10;
+    console.log(total);
+}
+
+function clearTotal(){
+    total=0;
+    console.log(total);
+}
+
+function plus(){
+    console.log('hello')
+}
